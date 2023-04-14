6e89f8373fb8dc8ea12f65e4298edc7bc02a8348
Jamile-Xavier <jamile.x.m@gmail.com>
2023-04-14 20:03:39 -0300

    Desafio finalizado v1.0

--- script.js
+++ script.js
@@ -3,15 +3,39 @@ let botaoDescriptografar = document.querySelector('.descriptografar');
 
 
 
-function criptografar() {
+function decodificar(codigo) {
   let textoDigitado = document.querySelector('.text-area-input').value;
-  textoDigitado.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+  let textoAlterado = '';
+  let inputResposta = document.querySelector('.change');
+  let outputResposta = document.querySelector('.output-answer ');
+  let textArea = document.querySelector('.text-area-output')
+
+  switch (codigo) {
+    case "criptografar":
+      textoAlterado = textoDigitado.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+      break;
+    case "descriptografar":
+      textoAlterado = textoDigitado.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+      break;
+    default:
+      console.log('Erro');
+      break;
+  }
+
+  if (inputResposta.classList.contains('change')) {
+    inputResposta.classList.replace('change', 'hide')
+
+    outputResposta.classList.remove('hide');
+    textArea.value = textoAlterado
+  }
 }
 
-function descriptografar() {
-  let textoDigitado = document.querySelector('.text-area-input').value;
-  textoDigitado.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+function copiarTexto() {
+  const texto = document.querySelector('.text-area-output');
+  texto.select();
+  document.execCommand('copy');
+  alert('Texto copiado com sucesso!')
 }
 
-botaoCriptografar.addEventListener('click', criptografar)
-botaoDescriptografar.addEventListener('click', descriptografar)
+botaoCriptografar.addEventListener('click', () => decodificar('criptografar'));
+botaoDescriptografar.addEventListener('click', () => decodificar('descriptografar'));
